3992034de74b9d0f4be406a38cb6e6deb9a26383
chinedu <chineduwobu@gmail.com>
2022-10-06 21:11:54 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -15,9 +15,9 @@ void *malloc_checked(unsigned int b)
 	void *p;
 
 
-	 p = malloc(b);
-	  if (p == NULL)
-		  exit(98);
-	  else
-		  return (p);
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	else
+		return (p);
 }
